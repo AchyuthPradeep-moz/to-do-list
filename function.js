9758d2259747078cafc779ddd0a3931aebdc2fa5
AchyuthPradeep-moz <achyuth@mozilor.com>
2026-03-13 10:42:09 +0530

    second change

--- function.js
+++ function.js
@@ -2,26 +2,43 @@ var tasks=[];
 var id=1;
 function addTask()
 {
-    var text=document.getElementById("add").value;
-    var task=
+    const input=document.getElementById("task")
+    const text=input.value;
+    text=document.getElementById("task").value;
+    const task=
     {
         id:id,
-        text:itemValue,
+        text:text,
         completed:false
 
     };
     tasks.push(task);
     id++;
+    input.value=""
+    saveTasks();
     display();
 }
 function display()
 {
-    var list=document.getElementById("list");
+    const list=document.getElementById("list");
     list.innerHTML="";
     for(var i=0;i<tasks.length;i++)
     {
-        var li=document.createElement("li");
-        li.innerText=tasks[i].text;
+        const li=document.createElement("li");
+        const checkbox=document.createElement("input");
+        checkbox.type="checkbox";
+        checkbox.checked=tasks[i].completed;
+        checkbox.addEventListener("change",function(){toggleTask(tasks[i].id)});
+        const span=document.createElement("span");
+        span.innerText=tasks[i].text;
+        span.addEventListener("dblclick",function(){editTask(tasks[i].id)});
+        const del=document.createElement("button");
+        del.innerText="Delete task";
+        del.addEventListener("click",function(){deleteTask(tasks[i].id)});
+        li.appendChild(checkbox);
+        li.appendChild(span);
+        li.append(del);
         list.appendChild(li);
     }
+
 }
