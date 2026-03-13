14b647ecbdef3881c0052c58e785bfae869d49da
AchyuthPradeep-moz <achyuth@mozilor.com>
2026-03-13 11:41:15 +0530

    third  change

--- function.js
+++ function.js
@@ -1,10 +1,10 @@
 var tasks=[];
 var id=1;
+document.getElementById("add").addEventListener("click",addTask);
 function addTask()
 {
-    const input=document.getElementById("task")
+    const input=document.getElementById("task");
     const text=input.value;
-    text=document.getElementById("task").value;
     const task=
     {
         id:id,
@@ -14,7 +14,7 @@ function addTask()
     };
     tasks.push(task);
     id++;
-    input.value=""
+    input.value="";
     saveTasks();
     display();
 }
@@ -24,21 +24,63 @@ function display()
     list.innerHTML="";
     for(var i=0;i<tasks.length;i++)
     {
+        const tasksid=tasks[i].id
         const li=document.createElement("li");
         const checkbox=document.createElement("input");
         checkbox.type="checkbox";
         checkbox.checked=tasks[i].completed;
-        checkbox.addEventListener("change",function(){toggleTask(tasks[i].id)});
+        checkbox.addEventListener("change",function(){toggleTask(tasksid)});
         const span=document.createElement("span");
         span.innerText=tasks[i].text;
-        span.addEventListener("dblclick",function(){editTask(tasks[i].id)});
+        span.addEventListener("dblclick",function(){editTask(tasksid)});
         const del=document.createElement("button");
         del.innerText="Delete task";
-        del.addEventListener("click",function(){deleteTask(tasks[i].id)});
+        del.addEventListener("click",function(){deleteTask(tasksid)});
         li.appendChild(checkbox);
         li.appendChild(span);
         li.append(del);
         list.appendChild(li);
     }
 
+}
+function toggleTask(tasksid)
+{
+    tasksid.completed=!tasksid.completed
+    saveTasks();
+    display();
+}
+function deleteTask(tasksid)
+{
+    tasks=tasks.filter(function(task)
+    {
+        return task.id != tasksid;
+    });
+    saveTasks();
+    display();
+}
+function editTask(tasksid)
+{
+    const newText=prompt("Edit task");
+    for(var i=0;i<tasks.length;i++)
+    {
+        if(tasks[i].id==tasksid)
+        {
+            tasks[i].text=newText;
+        }
+    }
+    saveTasks();
+    display();
+
+}
+document.getElementById("clear").addEventListener("click",clearCompleted);
+function clearCompleted()
+{
+    
+    tasks=tasks.filter(function(task){return task.completed ===false;});
+    saveTasks();
+    display();
+}
+function saveTasks()
+{
+    localStorage.setItem("tasks",JSON.stringify(tasks));
 }
